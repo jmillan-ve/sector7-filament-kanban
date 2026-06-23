34cbc676fac464af221a787ef3d04685eb96448c
Tu Nombre <you@example.com>
2026-06-23 12:21:41 -0400

    fix: pass view names as data variables instead of static property access

--- resources/views/filament-kanban/kanban-board.blade.php
+++ resources/views/filament-kanban/kanban-board.blade.php
@@ -1,11 +1,11 @@
 <x-filament-panels::page>
     <div x-data wire:ignore.self class="md:flex overflow-x-auto overflow-y-hidden gap-4 pb-4">
         @foreach($statuses as $status)
-            @include($this->statusView)
+            @include($statusView)
         @endforeach
 
         <div wire:ignore>
-            @include($this->scriptsView)
+            @include($scriptsView)
         </div>
     </div>
 
